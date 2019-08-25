d890d24aad6696010005e47cd698ed0c38c2240a
miki164 <miki3867@gmail.com>
2019-08-25 16:20:37 +0200

    Add APODProvider and examples

---
+++ providers/NASA/apod_provider.py
@@ -0,0 +1,26 @@
+from datetime import date
+
+import config
+from providers.api_provider import ApiProvider
+
+
+class APODProvider(ApiProvider):
+    """
+    Astronomy picture of the day. Requires NASA api key.
+    For more informations see https://api.nasa.gov/api.html#apod
+    """
+    def __init__(self):
+        super(ApiProvider).__init__()
+        self._api_url = "https://api.nasa.gov/planetary/apod"
+
+    @property
+    def api_key(self) -> str:
+        return config.get_api_keys('NASA')
+
+    def get_apod(self, date=date.today(), hd=False):
+        @self._get_request('', date=date, hd=hd)
+        def inner(response):
+            return response
+
+        return inner
+
